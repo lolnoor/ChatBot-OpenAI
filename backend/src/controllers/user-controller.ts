ec1cb8bf361babe9fa9e973af1394fb1b9649ba7
Nor Fat <n.fatima9797@gmail.com>
2024-08-21 02:22:07 +0300

    added authentication user management

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import {hash, compare} from "bcrypt";
 
 export const getAllUsers = async (
     req: Request,
@@ -17,17 +18,48 @@ export const getAllUsers = async (
 };
 
 
-export const usersignup = async (
+export const userSignup = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
     try {
-    //get all users (API)
-    const users = await User.find();
-    return res.status(200).json({ message: "OK", users });
-    } catch (error) {
+        //user signup
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({email});
+        if (existingUser) return res.status(401).send("User already exists.")
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(201).json({ message: "OK", id: user ._id.toString()});
+    }
+    catch (error) {
         console.log(error);
         return res.status(200).json({ message: "ERROR", cause: error.message });
-    } 
+    }
+};
+
+
+export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        //user login
+        const { email, password } = req.body;
+        const user = await User.findOne({email})
+        if (!user){
+            return res.status(401).send("Email is not registered")
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+        return res.status(403).send("Incorrect Password");
+        }
+    return res.status(201).json({ message: "OK", id: user ._id.toString()});
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 };
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllUsers, usersignup} from "../controllers/user-controller.js";
-
+import { getAllUsers, userSignup, userLogin} from "../controllers/user-controller.js";
+import {loginValidator, signupValidator, validate} from "../utils/validators.js"
 const userRoutes = Router();
 
-userRoutes.get("/", getAllUsers)
-userRoutes.post("/signup", usersignup)
+userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
+userRoutes.post("/login", validate(loginValidator), userLogin);
 
 export default userRoutes;
